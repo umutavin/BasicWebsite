4d7877d20902c904f4999dc5904115f7c74426b2
avin <umutavin@gmail.com>
2019-03-03 22:47:25 +0300

    all containers were implemented

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 jQuery(document).ready(function ($) {
-
-  $('#checkbox').change(function(){
-    setInterval(function () {
-        moveRight();
-    }, 3000);
-  });
+    
+    $("#toggle-button").click(function(){
+        $("#toggle-menu").toggle();
+    });
   
 	var slideCount = $('#slider ul li').length;
 	var slideWidth = $('#slider ul li').width();
